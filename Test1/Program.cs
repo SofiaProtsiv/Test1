5a7b1190de5ea937d408c132b97ed061215fa58c
sofia protsiv <prochivsofia@gmail.com>
2020-09-16 12:28:47 +0300

    Ввод/вывод

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -6,7 +6,29 @@ namespace Test1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello DonNU!");
+            /*string name = "Tom";
+            int age = 33;
+            bool isEmployed = false;
+            double weight = 78.65;*/
+
+            /* Console.WriteLine($"Name: {name} ");
+             Console.WriteLine($"Age: {age} ");
+             Console.WriteLine($"Weight: {weight} ");
+             Console.WriteLine($"Work: {isEmployed} ");*/
+
+            /*Console.WriteLine($"Name: {name} Age: {age} Weight: {weight} Work: {isEmployed}");*/
+
+            /*Console.WriteLine("Name: {0} Age: {2} Weight: {1}kg'', name, weight, age");*/
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            char age = (char)Console.Read();
+            Console.Write("Enter your height: ");
+            char height = (char)Console.Read();
+            Console.Write("Enter your salary: ");
+            char salary = (char)Console.Read();
+            Console.WriteLine("Name: {0} Age: {2} Height: {1}m Salary: {}$", name, age, height, salary);
         }
     }
 }
